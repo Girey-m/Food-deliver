8edce380e5f7915c256d47025255399890b2b6aa
Girey <larsvokul@mail.ru>
2024-09-22 21:59:16 +0300

    Практически закончил с стилизацией

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,4 @@
 import { initBurgerMenu } from "./modules/burger-menu";
 import { initOrderSwiper } from "./modules/swiper-order-place";
 initBurgerMenu();
-initOrderSwiper();
+// initOrderSwiper();
